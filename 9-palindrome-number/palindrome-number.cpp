013540da488003ad60131d14eec812761343d2be
Agraj Chandra <127575322+AgrajChandra@users.noreply.github.com>
2025-07-11 01:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (41.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+        // Special cases
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        long long reversed = 0;
-        long long temp = x;
-
-        while (temp != 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
+        int rev_half = 0;
+        while (x > rev_half) {
+            int last_digit = x % 10;
+            rev_half = rev_half * 10 + last_digit;
+            x = x / 10;
         }
 
-        return (reversed == x);
+        // For even digits: x == rev_half
+        // For odd digits:  x == rev_half / 10
+        return x == rev_half || x == rev_half / 10;
     }
 };
